ce79e4aff9a81f9747b553ceb55c0972dddf5570
xu chen <xuchen011@sina.com>
2013-11-21 17:34:20 +0800

    update

--- lottery/routes/index.js
+++ lottery/routes/index.js
@@ -1,8 +1,20 @@
-
+var cookie = require('../tools/cookie');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { });
+    res.render('index', { });
 };
+
+exports.doLottery = function(req, res){
+    var _cookieData = req.headers.cookie;
+    var thisCookie = new cookie(_cookieData);
+    var result = {};
+    var num = thisCookie.get('lotteryNum');
+    num = ( num == '' ) ? 3 : Math.floor(num);
+    result.success = num <= 0 ? false : true;
+    result.num = num;
+    res.writeHead(200, { 'Content-Type': 'application/json'} );
+    res.end( JSON.stringify( result ) );
+}
