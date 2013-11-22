6989e291434166e40ecf1ec2493f673659da9977
xu chen <xuchen011@sina.com>
2013-11-22 17:59:36 +0800

    update

--- lottery/app.js
+++ lottery/app.js
@@ -30,7 +30,10 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/view', routes.view);
+app.get('/lottery', routes.lottery);
 app.post('/doLottery', routes.doLottery);
+app.post('/canJoin', routes.canJoin);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- lottery/routes/index.js
+++ lottery/routes/index.js
@@ -7,6 +7,14 @@ exports.index = function(req, res){
     res.render('index', { });
 };
 
+exports.view = function(req, res){
+    res.send('view');
+};
+
+exports.lottery = function(req, res){
+    res.render('lottery', { });
+};
+
 exports.doLottery = function(req, res){
     var _cookieData = req.headers.cookie;
     var thisCookie = new cookie(_cookieData);
@@ -18,6 +26,16 @@ exports.doLottery = function(req, res){
     result.success = true;
     result.num = num;
     result.prize = arrPrize[prize];
+    res.writeHead(200, { 'Content-Type': 'application/json'} );
+    res.end( JSON.stringify( result ) );
+}
+
+exports.canJoin = function(req, res){
+    var result = {};
+    var url = ["view", "lottery"];
+    var prize = parseInt(Math.random()*(1-0+1)+0);
+    result.success = true;
+    result.url = url[prize];
     res.writeHead(200, { 'Content-Type': 'application/json'} );
     res.end( JSON.stringify( result ) );
 }
